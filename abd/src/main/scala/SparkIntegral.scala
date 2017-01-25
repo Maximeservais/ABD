f42fcef1e004b6e6548407768ba5205ca94c3a6c
Maximeservais <maximeservais@laposte.net>
2017-01-25 09:30:36 +0100

    ADD du projet et premiere version du calcul integral

---
+++ abd/src/main/scala/SparkIntegral.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SparkIntegral {
+
+  def main(args: Array[String]) {
+  
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    
+    val rectangleNumber = if (args.length > 0) args(0).toInt else 9
+    val slices = if (args.length > 0) args(1).toInt else 2
+    val areaLength = 9
+    val rectangleLength = areaLength / rectangleNumber
+    
+    val result = spark.sparkContext.parallelize(0 until rectangleNumber, slices).map { i =>
+      val rectangleHeight = 1 / (1 + i*rectangleLength)
+      val rectangleArea = rectangleHeight * rectangleLength
+    }.reduce(_ + _)
+
+    println("Integral is roughly " + result)
+    spark.stop()
+  }
+}
