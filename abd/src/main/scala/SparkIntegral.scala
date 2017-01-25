9bebff12c8be40f9384a76a71d00be84ec3e29c3
Maximeservais <maximeservais@laposte.net>
2017-01-25 14:01:09 +0100

    Integral fonctionnel avec relevé de temps

--- abd/src/main/scala/SparkIntegral.scala
+++ abd/src/main/scala/SparkIntegral.scala
@@ -9,16 +9,21 @@ object SparkIntegral {
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
     
-    val rectangleNumber = if (args.length > 0) args(0).toInt else 9
+    val rectangleNumber = if (args.length > 0) args(0).toFloat else 9
     val slices = if (args.length > 0) args(1).toInt else 2
     val areaLength = 9
-    val rectangleLength = areaLength / rectangleNumber
+    val rectangleLength = areaLength.toFloat / rectangleNumber.toFloat
     
-    val result = sc.parallelize(0 until rectangleNumber, slices).map { i =>
-      val rectangleHeight = 1 / (1 + i*rectangleLength)
-      rectangleHeight * rectangleLength
-    }.reduce(_ + _)
-
-    println("Integral is roughly " + result)
+    var start = System.currentTimeMillis();
+    for( j <- 1 to 10) {
+      val result = sc.parallelize(0 until rectangleNumber.toInt, slices).map { i =>
+        rectangleLength.toFloat / (1 + i.toFloat*rectangleLength.toFloat)
+      }.reduce(_ + _)
+      //println("Result : " + result)
+    }
+    var end = System.currentTimeMillis();
+    var tempsMoy = (end-start)/10
+    
+    println("Temps moyen de calcul : " + tempsMoy)
   }
 }
---
+++ abd/src/main/scala/di-grams.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SparkDigrams {
+
+  def main(args: Array[String]) {
+  
+    val conf = new SparkConf().setAppName("Digrams")
+    val sc = new SparkContext(conf)
+    
+    val rectangleNumber = if (args.length > 0) args(0).toInt else 9
+    val slices = if (args.length > 0) args(1).toInt else 2
+    val areaLength = 9
+    val rectangleLength = areaLength / rectangleNumber
+
+    
+      val result = sc.parallelize(0 until rectangleNumber, slices).map { i =>
+        val rectangleHeight = 1 / (1 + i*rectangleLength)
+        rectangleHeight * rectangleLength
+      }.reduce(_ + _)
+      
+    
+  }
+}
